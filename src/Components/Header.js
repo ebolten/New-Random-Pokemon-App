99b2580dc5d3baddcaa45f70d4cb947436a6138b
Emily Bolten <44903354+ebolten@users.noreply.github.com>
2023-07-20 09:27:23 -0400

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     render() {
         return(
             <div>
-                <img id='poke-image' src={'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png'} />
+                <img id='poke-image' alt="Pokemon" src={'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png'} />
                 <br />
             </div>
         )
